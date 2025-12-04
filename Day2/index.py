bfc391b817ffb074ee01f865532c644d82a4e5df
Alex Gawley <gawley@gmail.com>
2025-12-04 11:23:34 +0000

    Clean up day2

--- Day2/index.py
+++ Day2/index.py
@@ -3,13 +3,30 @@ import re
 with open("data.txt") as f:
     input = f.read()
 
-total = 0
-matcher = re.compile(r"^(\d+)\1+$")
+
+def part1():
+    total = 0
+    matcher = re.compile(r"^(\d+)\1$")
+
+    for item in input.split(","):
+        ends = [int(val) for val in item.split("-")]
+        for i in range(ends[0], ends[1] + 1):
+            if matcher.match(str(i)):
+                total += i
+    print(total)
 
 
-for item in input.split(","):
-    ends = [int(val) for val in item.split("-")]
-    for i in range(ends[0], ends[1] + 1):
-        if matcher.match(str(i)):
-            total += i
-print(total)
+def part2():
+    total = 0
+    matcher = re.compile(r"^(\d+)\1+$")
+
+    for item in input.split(","):
+        ends = [int(val) for val in item.split("-")]
+        for i in range(ends[0], ends[1] + 1):
+            if matcher.match(str(i)):
+                total += i
+    print(total)
+
+
+part1()
+part2()
